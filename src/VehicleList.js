af3c1a6b5f9c34e675b1eb0f85838e27d2433a85
Riley Hoffman <rileyjhoff@gmail.com>
2022-04-27 15:13:35 -0700

    VehicleList now maps over vehicles prop and renders Vehicle components

--- src/VehicleList.js
+++ src/VehicleList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Vehicle from './Vehicle';
 
 export default function VehicleList({ vehicles }) {
   return (
@@ -6,6 +7,9 @@ export default function VehicleList({ vehicles }) {
       {/* this component takes in a prop called vehicles, which is an array of vehicle strings */}
       {/* map through the vehicles array in props */}
       {/* for each item render a Vehicle component. Pass the vehicle string as a prop called vehicle to the Vehicle component.  */}
+      {vehicles.map(vehicle => 
+        <Vehicle key={vehicle} vehicle={vehicle} />
+      )}
     </div>
   );
 }
