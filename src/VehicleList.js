9490c724e7595e51ff2400428521c16897a89436
Riley Hoffman <rileyjhoff@gmail.com>
2022-04-27 17:46:14 -0700

    gave Vehicles unique keys and some starter code to play around with adding cars to traffic based on time spent on page

--- src/VehicleList.js
+++ src/VehicleList.js
@@ -7,8 +7,8 @@ export default function VehicleList({ vehicles }) {
       {/* this component takes in a prop called vehicles, which is an array of vehicle strings */}
       {/* map through the vehicles array in props */}
       {/* for each item render a Vehicle component. Pass the vehicle string as a prop called vehicle to the Vehicle component.  */}
-      {vehicles.map(vehicle => 
-        <Vehicle key={vehicle} vehicle={vehicle} />
+      {vehicles.map((vehicle, i) => 
+        <Vehicle key={vehicle + i} vehicle={vehicle} />
       )}
     </div>
   );
